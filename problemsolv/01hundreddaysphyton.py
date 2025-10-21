e3d47a8a139ef6832ad7a4d4678f355ceccb7bec
paladinolucas <lucasfabris2033@gmail.com>
2025-10-21 19:48:16 +0100

    teste

---
+++ problemsolv/01hundreddaysphyton.py
@@ -0,0 +1,23 @@
+""" 
+Class 18/10/2025 - Ahmira
+
+# class 01 - Phyton
+
+"""
+
+print("Hello, World!")
+print(6)
+print(5.6)
+print(False)
+print ("Lucas")
+print("Lucas", 5, True)
+print("India", "Pakistan", "Srilanka", sep='/')
+print("India", "Pakistan", "Srilanka", sep='-')
+
+print("Hello")
+print("World")
+
+print("Hello", end=' ')
+print("World")
+
+
