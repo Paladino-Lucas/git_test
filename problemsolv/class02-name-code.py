aaae19f0622c5020dcaa5de393f98461e3197cc2
paladinolucas <lucasfabris2033@gmail.com>
2025-10-21 20:45:11 +0100

    AZSXX

--- problemsolv/class02-name-code.py
+++ problemsolv/class02-name-code.py
@@ -3,7 +3,6 @@ a= 5
 b= 10
 text = input("Enter a word for your banner:") .strip()
 width = len(text) + 14 
-
 print(f" the sum of {a} and {b} is {a+b}.")
 print(f"Nice to meet you, {name}.")
        # the "f" is called f-string
