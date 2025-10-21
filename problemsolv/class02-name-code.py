d4dd399959727e9917ece53f150ff016aa38955f
paladinolucas <lucasfabris2033@gmail.com>
2025-10-21 20:40:49 +0100

    f-string

--- problemsolv/class02-name-code.py
+++ problemsolv/class02-name-code.py
@@ -1,5 +1,12 @@
-name = input("enter your name")
-print (f"Nice to meet you, {name}.")
+name = input("Enter your name: ").strip()
+a= 5
+b= 10
+text = input("Enter a word for your banner:") .strip()
+width = len(text) + 14 
 
-
-
+print(f" the sum of {a} and {b} is {a+b}.")
+print(f"Nice to meet you, {name}.")
+       # the "f" is called f-string
+print("*" * width) 
+print(f"*{text}*")
+print("*" * width) 
